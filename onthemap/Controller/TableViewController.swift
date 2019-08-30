45a222c4ad641fbdaf5afdb49799f20908685f6f
Peter Burgner <dev@burgner.de>
2019-08-29 22:21:45 -0700

    Add empty TableViewController and segues

---
+++ onthemap/Controller/TableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TableViewController.swift
+//  onthemap
+//
+//  Created by Peter Burgner on 8/29/19.
+//  Copyright © 2019 Peter Burgner. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UIViewController {
+    
+}
