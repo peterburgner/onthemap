578acbb0e67bbd13e7e6ed282ee11fe57232b0ad
Peter Burgner <dev@burgner.de>
2019-08-29 21:54:29 -0700

    Customize map pins and open URLs

--- onthemap/Controller/MapViewController.swift
+++ onthemap/Controller/MapViewController.swift
@@ -75,6 +75,42 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         
     }
     
+    // MARK: - MKMapViewDelegate
+    
+    // Here we create a view with a "right callout accessory view". You might choose to look into other
+    // decoration alternatives. Notice the similarity between this method and the cellForRowAtIndexPath
+    // method in TableViewDataSource.   
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+
+        let reuseId = "pin"
+
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            pinView!.pinTintColor = .red
+            pinView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+        else {
+            pinView!.annotation = annotation
+        }
+
+        return pinView
+    }
+    
+    // This delegate method is implemented to respond to taps. It opens the system browser
+    // to the URL specified in the annotationViews subtitle property.
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if control == view.rightCalloutAccessoryView {
+            let app = UIApplication.shared
+            if let toOpen = view.annotation?.subtitle! {
+                app.open(URL(string:toOpen)!, options: [:], completionHandler: nil)
+            }
+        }
+    }
+    
+    
     func handleLogoutResponse(success: Bool, error: Error?) {
         if success {
             performSegue(withIdentifier: "login", sender: nil)
