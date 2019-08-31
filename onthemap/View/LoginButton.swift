af921fc4991d20089886ccc007ab8040bf21b5bb
Peter Burgner <dev@burgner.de>
2019-08-30 23:51:31 -0700

    Fix folder structure

---
+++ onthemap/View/LoginButton.swift
@@ -0,0 +1,19 @@
+//
+//  LoginButton.swift
+//  onthemap
+//
+//  Created by Peter Burgner on 8/27/19.
+//  Copyright © 2019 Peter Burgner. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginButton: UIButton {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.cornerRadius = 5
+    }
+    
+}
