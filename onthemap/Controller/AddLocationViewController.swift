542371e243487c408771d2963173dd00d2324edb
Peter Burgner <dev@burgner.de>
2019-08-31 00:00:47 -0700

    Implement cancel button

---
+++ onthemap/Controller/AddLocationViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AddLocationViewController.swift
+//  onthemap
+//
+//  Created by Peter Burgner on 8/30/19.
+//  Copyright © 2019 Peter Burgner. All rights reserved.
+//
+
+import UIKit
+
+class AddLocationViewController: UIViewController {
+    
+    @IBAction func cancel(_ sender: Any) {
+        self.dismiss(animated: false) {
+        }
+    }
+}
